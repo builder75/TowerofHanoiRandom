df1130f0394145caf40552b8e2f65319a353c130
builder75 <builder75@example.com>
2022-06-20 12:51:20 +0200

    enter number of disks

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,34 @@
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
     static int count = 0;
 
     public static void main(String[] args) {
-        Stack<String> stack = new Stack<>();
-        Stack<String> stack1 = new Stack<>();
-        Stack<String> stack2 = new Stack<>();
-        Stack<String> stack3 = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        Stack<Integer> stack3 = new Stack<>();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter number of disks");
+
+        int numberOfDisks = scanner.nextInt();
+
+        for (int i = numberOfDisks; i > 0; i--) {
+            stack.push(i);
+            stack1.push(i);
 
 
-        // compare result
-        stack.push("F");
-        stack.push("E");
-        stack.push("D");
-        stack.push("C");
-        stack.push("B");
-        stack.push("A");
-
-        // start
-        stack1.push("F");
-        stack1.push("E");
-        stack1.push("D");
-        stack1.push("C");
-        stack1.push("B");
-        stack1.push("A");
+        }
 
 
         while (!stack.equals(stack3)) {
             double rand = Math.random();
             if (rand < 1.0 / 6 && !stack1.isEmpty()) {
                 if (!stack2.isEmpty()) {
-                    if (stack1.peek().charAt(0) < stack2.peek().charAt(0)) {
+                    if (stack1.peek() < stack2.peek()) {
                         stack2.push(stack1.pop());
                     }
                 } else {
@@ -44,7 +40,7 @@ public class Main {
 
             } else if (rand >= 1.0 / 6 && rand < 2.0 / 6 && !stack1.isEmpty()) {
                 if (!stack3.isEmpty()) {
-                    if (stack1.peek().charAt(0) < stack3.peek().charAt(0)) {
+                    if (stack1.peek() < stack3.peek()) {
                         stack3.push(stack1.pop());
                     }
                 } else {
@@ -55,7 +51,7 @@ public class Main {
 
             } else if (rand >= 2.0 / 6 && rand < 3.0 / 6 && !stack2.isEmpty()) {
                 if (!stack1.isEmpty()) {
-                    if (stack2.peek().charAt(0) < stack1.peek().charAt(0)) {
+                    if (stack2.peek() < stack1.peek()) {
                         stack1.push(stack2.pop());
                     }
                 } else {
@@ -67,7 +63,7 @@ public class Main {
 
             } else if (rand >= 3.0 / 6 && rand <= 4.0 / 6 && !stack2.isEmpty()) {
                 if (!stack3.isEmpty()) {
-                    if (stack2.peek().charAt(0) < stack3.peek().charAt(0)) {
+                    if (stack2.peek() < stack3.peek()) {
                         stack3.push(stack2.pop());
                     }
                 } else {
@@ -79,7 +75,7 @@ public class Main {
 
             } else if (rand >= 4.0 / 6 && rand < 5.0 / 6 && !stack3.isEmpty()) {
                 if (!stack1.isEmpty()) {
-                    if (stack3.peek().charAt(0) < stack1.peek().charAt(0)) {
+                    if (stack3.peek() < stack1.peek()) {
                         stack1.push(stack3.pop());
                     }
                 } else {
@@ -91,7 +87,7 @@ public class Main {
 
             } else if (rand >= 5.0 / 6 && !stack3.isEmpty()) {
                 if (!stack2.isEmpty()) {
-                    if (stack3.peek().charAt(0) < stack2.peek().charAt(0)) {
+                    if (stack3.peek() < stack2.peek()) {
                         stack2.push(stack3.pop());
                     }
                 } else {
